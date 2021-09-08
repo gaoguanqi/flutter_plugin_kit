fcbf9163b85d4b14bc7879e5dec43cbc5aabd73e
gaoguanqi <307590625@qq.com>
2021-09-08 18:18:04 +0800

    解决  FlutterPluginKitPlugin 爆红

--- lib/flutter_plugin_kit.dart
+++ lib/flutter_plugin_kit.dart
@@ -7,6 +7,7 @@ class FlutterPluginKit {
   static const MethodChannel _channel =
       const MethodChannel('flutter_plugin_kit');
 
+
   static Future<String?> get platformVersion async {
     final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
